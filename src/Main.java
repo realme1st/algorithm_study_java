e475335bfde5d5d205be9086b483acc8b7fa4012
이성민 <ll8470@naver.com>
2022-01-17 22:49:37 +0900

    백준 1316

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n= Integer.parseInt(br.readLine());
+		int count =0;
+		
+		
+		
+		for(int i=0;i<n;i++) {
+			String s = br.readLine();
+			boolean check[]=new boolean[26];
+			boolean tmp = true;
+			
+			for(int j=0;j<s.length();j++) {
+				if(check[s.charAt(j)-'a']) {
+					if(s.charAt(j)!=s.charAt(j-1)) {
+						tmp=false;
+						break;
+					}
+				}else {
+					check[s.charAt(j)-'a']=true;
+				}
+				
+			}
+			if(tmp)
+				count++;
+		}
+		
+		bw.write(count+"\n");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+		
+		
+	}
+}
