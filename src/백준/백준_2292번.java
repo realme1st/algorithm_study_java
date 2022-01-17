373cd07b3b435805ee7316473c0f95256139180e
이성민 <ll8470@naver.com>
2022-01-18 08:51:07 +0900

    백준 2292

--- src/백준/백준_2292번.java
+++ src/백준/백준_2292번.java
@@ -1,10 +1,30 @@
 package 백준;
-
+import java.util.*;
+import java.io.*;
 public class 백준_2292번 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int count=1;
+		int range=2;
+		
+		if(n==1) {
+			bw.write(1+"\n");
+		}else {
+			while(range<=n) {
+				range=range+(6*count);
+				count++;
+			}
+			bw.write(count+"\n");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
 	}
 
 }
